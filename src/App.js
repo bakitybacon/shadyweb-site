eed55ff56af0176dba68bec9380f74a2ddb1538c
Corrin Fosmire <coryfosmire@gmail.com>
2019-08-05 12:29:04 -0500

    modify css

--- src/App.js
+++ src/App.js
@@ -4,21 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Welcome to the Shady Website!</h2>
+      <br />
+      <h2>Check back soon for more updates!</h2>
     </div>
   );
 }
