bd9b69c6d66c05f1fe40698a6f0bc65759cfc026
Corrin Fosmire <coryfosmire@gmail.com>
2019-08-08 23:46:57 -0500

    update homepage

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h2>Welcome to the Shady Website!</h2>
-      <br />
-      <h2>Check back soon for more updates!</h2>
+    <div class="center-div">
+      <center> <h2>Corrin Fosmire</h2> </center>
+      <img src="me.jpg" height="683" width="512"></img>
+      <center> <h2> <a href="/CorrinFosmireResume.pdf"> Resume </a> | <a href="https://github.com/bakitybacon"> GitHub </a> | <a href="mailto:cf24@rice.edu">Email </a> </h2> </center>
     </div>
   );
 }
